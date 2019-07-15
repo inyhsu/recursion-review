98cdc288796fd7af6faca493f4dfce2647ead3fa
inyhsu <isingridsu@gmail.com>
2019-07-15 11:56:53 -0700

    update

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,33 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  //if boolean -> return '' + boolean
+  //if number -> return '' + number
+    //if number is Infinity and NaN or null -> return null
+  //if string -> return '"' + string + '"'
+  //if undefined -> return undefined
+
+  //if undefined, function, or symbol
+    //if in object -> return ommitted
+    //if in array -> return null
+    
+    
+  if(typeof obj === 'boolean' || typeof obj === 'number'){
+    if(obj === Infinity || obj === NaN || obj === null){
+      return null
+    }
+    
+    return '' + obj 
+  }
+  
+  if(typeof obj === 'string'){
+    return '"' + obj + '"'
+  }
+  
+  if(typeof obj === 'undefined'){
+    return undefined
+  }
+  
+  
 };
