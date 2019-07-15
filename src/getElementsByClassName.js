de1ca4c064919a8efdf1c6f02e159570027628fd
inyhsu <isingridsu@gmail.com>
2019-07-15 16:25:57 -0700

    partial getElementsByClassName.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  // You should use document.body, element.childNodes, and element.classList
+  
+  /*
+    Create an empty array and push the element that has the className
+      Have body as document.body.childNodes then if className === className, push document.body.childNodes.className;
+      recurse into deeper childNodes
+  */
+  
+  let results = [];
+  let bodyChildren = document.body.childNodes;
+  
+  function moving(className, bodyChildren){
+    for(let i = 0; i < bodyChildren.length ; i++){
+      if(bodyChildren[i].className === className && bodyChildren[i] !== undefined){
+        results.push(bodyChildren[i]);
+      } else {
+        return results.concat(moving(className, bodyChildren[i]));
+      }
+    }
+  }
+  
+  moving(className, bodyChildren);
+  
+  return results;
 };
